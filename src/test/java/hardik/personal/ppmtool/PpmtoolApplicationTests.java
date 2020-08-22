ed2c02f2530c5dd404e5632e98b0005cf6c648b7
hardiksingh98 <hardik.singh@turtlemint.com>
2020-08-22 18:32:39 +0530

    first commit

---
+++ src/test/java/hardik/personal/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package hardik.personal.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
